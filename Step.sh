29cd350c279ff63582a559cfaa126c4181268413
EBLETSM <86255713+EBLETSM@users.noreply.github.com>
2021-07-18 20:31:27 +0430

    Add files via upload

---
+++ Step.sh
@@ -0,0 +1,9 @@
+echo "Loding......"
+pip install os
+pip install requests
+pip install random
+pip install time
+pip install sys
+pip install colorama
+echo "[~] OK install End [~]"
+python3 instabot.py
