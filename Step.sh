6b9e388176ef58251e398df5b98e9c61f7da7257
EBLETSM <86255713+EBLETSM@users.noreply.github.com>
2021-07-18 12:13:16 -0400

    Delete Step.sh

--- Step.sh
+++
@@ -1,9 +0,0 @@
-echo "Loding......"
-pip install os
-pip install requests
-pip install random
-pip install time
-pip install sys
-pip install colorama
-echo "[~] OK install End [~]"
-python3 instabot.py
