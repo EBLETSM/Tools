3e6ea8c12aadf07521ed24ed43a50fe55cc56c1e
HAXA-XC <86255713+EBLETSM@users.noreply.github.com>
2021-09-04 14:21:54 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+#CipherX
+echo "install Run- #CipherX"
+clear
+pkg install python
+pip install os
+pip install colorama
+pip install requests
+pip install sys
+pip install pyngrok
+pip install socket
+clear
+python3 CipherX.py
+# 81 x 25
